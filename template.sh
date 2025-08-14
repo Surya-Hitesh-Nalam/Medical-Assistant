3d44f19d09fe767afdae215a20724a90e247950f
Surya-Hitesh-Nalam <suryanalam1234@gmail.com>
2025-08-14 19:26:01 +0530

    first commit

---
+++ template.sh
@@ -0,0 +1,19 @@
+#creating directories for project structure
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "Directories and files created successfully."
+
+
+
+
