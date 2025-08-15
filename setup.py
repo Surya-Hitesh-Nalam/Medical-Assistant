857257d2eaf0a9af0ab1f774f65a42a75bdd0d86
Surya-Hitesh-Nalam <suryanalam1234@gmail.com>
2025-08-15 15:37:44 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='gym',
+    version='0.0.1',
+    author='surya hitesh',
+    autgor_email='suryanalam1234@gamil.com',
+    packages=find_packages(),
+    install_requires=[]
+)
