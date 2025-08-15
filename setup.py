7908cfe178592d83443b9781c158f615218ec6f4
Surya-Hitesh-Nalam <suryanalam1234@gmail.com>
2025-08-15 17:32:45 +0530

    research_done

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='gym',
+    name='medical Bot',
     version='0.0.1',
     author='surya hitesh',
     autgor_email='suryanalam1234@gamil.com',
